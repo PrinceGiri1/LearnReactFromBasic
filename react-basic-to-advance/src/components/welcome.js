4bd7008adc74bff9d368515faf72feadb94e55a1
PrinceGiri1 <princegiri262@gmail.com>
2023-09-10 11:56:30 +0530

    Learn functional and class component in detail

---
+++ react-basic-to-advance/src/components/welcome.js
@@ -0,0 +1,12 @@
+// Import React and Component from 'react'
+import React, { Component } from 'react';
+
+// Define your class component
+class Welcome extends Component {
+    // Render method should return JSX
+    render() {
+        return <h1>This is class component</h1>;
+    }
+}
+
+export default Welcome;
